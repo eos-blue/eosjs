58ae914f5281d9a58993e4d8b0994d28a45f31de
aleclarson <alec.stanford.larson@gmail.com>
2018-07-02 18:53:04 -0400

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+const pkg = require('./package.json');
+
+module.exports = {
+  input: 'src/index.js',
+  external: Object.keys(pkg.dependencies),
+  output: {
+    file: pkg.main,
+    format: 'cjs'
+  },
+  plugins: [
+    require('rollup-plugin-ignore')([
+      'assert',
+    ]),
+    require('rollup-plugin-node-resolve')(),
+    require('rollup-plugin-commonjs')(),
+    require('rollup-plugin-strip')({
+      functions: ['console.*', 'assert', 'assert.*']
+    }),
+    require('rollup-plugin-json')(),
+    require('rollup-plugin-buble')(),
+  ]
+};
